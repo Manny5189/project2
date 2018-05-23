0f15641af8034f52fbab525f4722f3eee908df68
kburt <kellie.burt@gmail.com>
2018-05-22 21:15:02 -0500

    updated index.html

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-function outputUpdate(num) {
-    document.querySelector('#output').value = num;
+function outputUpdateRating(num) {
+    document.querySelector('#outputRating').value = num;
     }
+
+function outputUpdateDuration(num) {
+    document.querySelector('#outputDuration').value = num;
+    }
+
+$(document).ready(function(){
+        $( "a.theCriteria" ).click(function( event ) {
+            event.preventDefault();
+            $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 900);
+        });
+    });
